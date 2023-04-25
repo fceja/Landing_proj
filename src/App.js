b2fad03c77b5a92744a890c5f8c61613685d6236
FC <fceja@FCs-MacBook-Pro.lan>
2023-04-24 21:55:16 -0700

    add components to main App file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './styles/App.css';
+
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import RowButtons from './components/RowButtons';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Navbar/>
+      <RowButtons/>
+      <Footer/>
     </div>
   );
 }
