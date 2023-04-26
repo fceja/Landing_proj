f0d67b73602b36733c02994abde036a67bd1aa5e
FC <fceja@FCs-MacBook-Pro.lan>
2023-04-25 18:01:36 -0700

    add HamburgerMenu component w/ animation

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,5 +1,33 @@
-export default function HamburgerMenu () {
-    return(
-        <div>This is HamburgerMenu</div>
-    )
-}
+import "../css/components/HamburgerMenu.css";
+
+import React, { useState } from "react";
+
+const HamburgerMenu = () => {
+  const [burgerClass, setBurgerClass] = useState("hamburger-bar unclicked");
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("hamburger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("hamburger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
+
+  return (
+    <div className="hamburger-menu-container">
+      <div className="hamburger-menu-bar-container" onClick={updateMenu}>
+        <div className={burgerClass}></div>
+        <div className={burgerClass}></div>
+        <div className={burgerClass}></div>
+      </div>
+      <div className={menuClass}></div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
