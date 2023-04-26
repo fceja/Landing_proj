04fbfe3a2435ccb8b45937ab8b38fa2aff1e6398
fceja <cisco_ceja@icloud.com>
2023-04-25 18:07:17 -0700

    add css,bootstrap, and new comp to App file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './styles/App.css';
+import './css/App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import RowButtons from './components/RowButtons';
 
 function App() {
