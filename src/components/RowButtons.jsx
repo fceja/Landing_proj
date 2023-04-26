25f1a6e4260c11fc82b6501c036140e8b366c8f3
FC <fceja@FCs-MacBook-Pro.lan>
2023-04-25 18:04:16 -0700

    add btns into container

--- src/components/RowButtons.jsx
+++ src/components/RowButtons.jsx
@@ -1,9 +1,9 @@
 export default function RowButtons() {
   return (
-    <>
+    <div className='row-btns'>
       <button>button1</button>
       <button>button2</button>
       <button>button3</button>
-    </>
+    </div>
   );
 }
