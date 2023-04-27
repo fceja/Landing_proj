1a619fa5b600af6b465d6458d95c20cb2331f1b4
fceja <cisco_ceja@icloud.com>
2023-04-26 22:11:26 -0700

    remove log

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import "../css/components/Footer.css";
 export default function Footer() {
   const currentDate = new Date();
   const year = currentDate.toLocaleDateString(undefined, { year: "numeric" });
-  console.log(year);
 
   return <footer>&copy; {year}, Company</footer>;
 }
